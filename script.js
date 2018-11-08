cb779eb04fac223af49639a08e21bb70567ba689
Robert Claus <robertclaus@gmail.com>
2018-11-08 09:45:06 -0600

    Topic Grouping!

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ function createBubbleChart(error, entries) {
               var groupIndex = topicDomain.indexOf(d[secondaryKey]);
 
               var rowCount = Math.floor(groupIndex%rowLength);
-              var columnCount = Math.floor((rowCount*rowLength)%columnLength);
+              var columnCount = groupIndex - Math.floor((rowCount*rowLength)%columnLength);
 
               var perColumnHeight = height/columnLength;
 
